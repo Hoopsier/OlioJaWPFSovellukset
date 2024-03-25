92073397cff72085ac9190184d4f44c9dfb622f0
Hoopsy <renanhoruz@gmail.com>
2024-03-25 10:04:54 +0200

    H5 test

---
+++ Harjoitus 5/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Renan Horuz
+ */
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int lasku;  //Mikä lasku 1-4
+        float x;    //Ensimmäinen numero
+        float y;    //Toinen numero
+        Console.WriteLine("1 = Summa\n2 = Erotus\n3 = Tulo\n4 = Osamäärä");
+        Console.Write("Valitsemasi lasku: ");
+        lasku = Convert.ToInt(Console.ReadLine());
+
+        Console.Write("Seuraavaksi ensimmäinen luku:");
+        x = Convert.ToFloat(Console.ReadLine());
+
+        Console.Write("Viimeiseksi toinen luku:");
+        y = Convert.ToFloat(Console.ReadLine());
+
+        switch (lasku)
+        {
+            case 1: //Summa
+                Console.Write(x + " + " + y + " = ");
+                Console.WriteLine(x + y);
+            break; 
+            
+            case 2: //Erotus
+                Console.Write(x + " - " + y + " = ");
+                Console.WriteLine(x - y);
+            break; 
+            
+            case 3: //Tulo
+                Console.Write(x + " * " + y + " = ");
+                Console.WriteLine(x * y);
+            break; 
+            
+            case 4: //Osamäärä
+                Console.Write(x + " / " + y + " = ");
+                Console.WriteLine(x / y);
+            break;
+        }
+    }
+}
