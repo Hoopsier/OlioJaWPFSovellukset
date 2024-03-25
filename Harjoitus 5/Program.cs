9f60a58011ab2107452e2b0e8526d65195e26934
Hoopsy <renanhoruz@gmail.com>
2024-03-25 10:20:39 +0200

    H5 Valmis

--- Harjoitus 5/Program.cs
+++ Harjoitus 5/Program.cs
@@ -6,39 +6,47 @@ internal class Program
     private static void Main(string[] args)
     {
         int lasku;  //Mikä lasku 1-4
-        float x;    //Ensimmäinen numero
-        float y;    //Toinen numero
+        int x;      //Ensimmäinen numero
+        int y;      //Toinen numero
+        int z = 0;      //Vastaus
+
+        //Ohjaus
         Console.WriteLine("1 = Summa\n2 = Erotus\n3 = Tulo\n4 = Osamäärä");
         Console.Write("Valitsemasi lasku: ");
-        lasku = Convert.ToInt(Console.ReadLine());
+        lasku = Convert.ToInt32(Console.ReadLine());
 
         Console.Write("Seuraavaksi ensimmäinen luku:");
-        x = Convert.ToFloat(Console.ReadLine());
+        x = Convert.ToInt32(Console.ReadLine());
 
         Console.Write("Viimeiseksi toinen luku:");
-        y = Convert.ToFloat(Console.ReadLine());
+        y = Convert.ToInt32(Console.ReadLine());
 
         switch (lasku)
         {
             case 1: //Summa
                 Console.Write(x + " + " + y + " = ");
-                Console.WriteLine(x + y);
+                z = x + y;
+
             break; 
             
             case 2: //Erotus
                 Console.Write(x + " - " + y + " = ");
-                Console.WriteLine(x - y);
+                z = x - y;
+
             break; 
             
             case 3: //Tulo
                 Console.Write(x + " * " + y + " = ");
-                Console.WriteLine(x * y);
-            break; 
+                z = x * y;
+
+                break; 
             
             case 4: //Osamäärä
                 Console.Write(x + " / " + y + " = ");
-                Console.WriteLine(x / y);
+                z = x / y; 
+
             break;
         }
+        Console.WriteLine(z);   //Vastaus tekstinä
     }
 }
