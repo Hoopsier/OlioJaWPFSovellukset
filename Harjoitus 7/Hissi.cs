fe3a85a0f1eaf0044ac7b24d03cbd57bc653aa6c
Hoopsy <renanhoruz@gmail.com>
2024-03-28 10:44:53 +0200

    Harjoitus 7

---
+++ Harjoitus 7/Hissi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_7
+{
+    internal class Hissi
+    {
+        public static int kerros {get; set;}
+        public static bool done = false;
+        public static void KerrosSiirto()
+        {
+            Console.WriteLine("Valitse kerros (1-6)");
+            
+
+            while (Hissi.done == false)
+            {
+                kerros = int.Parse(Console.ReadLine());
+                if (kerros < 1 || kerros > 6)
+                {
+                    Console.WriteLine("Ei sopiva kerros, valitse näistä (1-6)");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Hissi on " + kerros + ". kerroksessa.");
+                    
+                }
+            }
+        }
+    }
+}
---
+++ Harjoitus 7/Program.cs
@@ -0,0 +1,14 @@
+﻿using Harjoitus_7;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Hissi hissi = new Hissi();
+        while (!Hissi.done)
+        {
+            Hissi.KerrosSiirto();
+        }
+
+    }
+}
