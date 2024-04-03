3b25e8b370b229d1487779675f69cd5f146958a2
Hoopsy <renanhoruz@gmail.com>
2024-04-03 14:15:01 +0300

    Harjoitus 11 (Ilman videota vieläkin)

---
+++ Harjoitus 11/Opiskelija.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_11
+{
+    internal class Opiskelija
+    {
+        //opiskelijan muuttujat
+        public string etunimi;
+        public string sukunimi;
+        public string ryhmatunnus;
+        public string opID { get; set; }
+        //opiskelija konstruktori
+        public Opiskelija(string _enimi, string _snimi, string _rtunnus, string? _opID)
+        {
+            etunimi = _enimi;
+            sukunimi = _snimi;
+            ryhmatunnus = _rtunnus;
+            opID = _opID;
+        }
+    }
+}
--- Harjoitus 11/Program.cs
+++ Harjoitus 11/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Renan Horuz & help of gpt
+ * EI VIDEOITA KÄYTETTYNÄ TEHTÄVIIN (en jaksa tehä kotitehtäviä JA harjoituksia täysin yksin)
+ * tää tehtävä on varmasti huomattavan erillainen videosta
+ */
+using Harjoitus_11;
+using System.Collections;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool x = false;
+        Dictionary<string, Opiskelija> opiskelijaSanakirja = new Dictionary<string, Opiskelija>();
+        Opiskelija Matti = new Opiskelija("Matti", "Mykkä", "RAT25","1");
+        opiskelijaSanakirja.Add(Matti.opID, Matti);
+        //guidance
+        Console.WriteLine("Please input a new student in this format: fName + enter + lName + enter + group + enter + studentID");
+
+
+
+        Opiskelija Matti1 = new Opiskelija(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(),null);
+        while (!x)
+        {
+            try
+            {
+                Matti1.opID = Console.ReadLine();
+                //opiskelijaSanakirja.Add(Matti1.opID, Matti1); Tämä lisäisi indexin vaikka siinä olisi samanlainen avain, täysin rikkoo tämän.
+                //this portion was assisted by gpt. However, it didn't follow guidelines.
+                if (opiskelijaSanakirja.ContainsKey(Matti1.opID)) //error check
+                {
+                    throw new MyException("ID has already been assigned. Try again please.");
+                }
+                else
+                {
+                    opiskelijaSanakirja.Add(Matti1.opID, Matti1);
+                    x = true; // Set x to true to end the loop
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("That ID is already in use! Try again please.");
+            }
+        }
+
+        Console.WriteLine(Matti.etunimi+" id: "+Matti.opID
+            +"\n"+Matti1.etunimi+" id: "+Matti1.opID);
+
+    }
+}
