db05df3526703d3774bf1315dfed471a67ba5e81
Hoopsy <renanhoruz@gmail.com>
2024-05-20 14:30:38 +0300

    KT3

---
+++ KT3/Kiuas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT3
+{
+    public class Kiuas
+    {
+        //variables
+        public int temp;
+        public int moist;
+        public bool on;
+        public Kiuas(int _temp, int _moist, bool _on) //constructor
+        {
+            this.temp = _temp;
+            this.moist = _moist;
+            on = _on;
+        }
+        public void ToggleKiuas()
+        {
+            on = !on; // the toggle
+            if (on == true) // toggle's effects
+            {
+                Console.WriteLine("Kiuas on päällä.");
+                
+            }
+            else
+            {
+                temp = 20;
+                moist = 0;
+                Console.WriteLine("Kiuas on pos päältä.");
+            }
+        }
+
+
+    }
+}
---
+++ KT3/Program.cs
@@ -0,0 +1,85 @@
+﻿using KT3;
+/*
+ * Renan Horuz
+ */
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Kiuas kiuas = new Kiuas(20, 0, false);
+        Console.WriteLine("Kiuas on poissa päältä. pistääksesi sen päälle, kirjoita y");
+        
+        while (true)
+        {
+            var x = Console.ReadLine();
+            if (x == "y")
+            {
+                kiuas.ToggleKiuas();
+                
+                break;
+            }
+           else
+            {
+                Console.WriteLine("Vastaus ei ollut y. Jos haluat laittaa kiukaan päälle, kirjoita y");
+            }
+        }
+        Console.WriteLine("Seuraavaksi minkä haluat asettaa (l)ämpötilan vai (K)osteuden?");
+        var a = false;
+        while (!a)
+        {
+
+            var x = Console.ReadLine();
+            if (x == "k")
+            {
+                Console.WriteLine("Minkä kosteuden haluat asettaa? 0-100 %.");
+                while (true) //varmistaa että saa tuloksen
+                {
+                    var y = Console.ReadLine();
+                    if (int.TryParse(y, out int z))
+                    {
+                        z = Math.Clamp(z, 0, 100);
+                        Console.WriteLine("Kosteus on nyt " + z + " %.");
+                        kiuas.moist = z;
+                        Console.WriteLine("Oletko tämä sopiva? (y)");
+                        var b = Console.ReadLine();
+                        if (b == "y") { break; }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vain numeroita kiitos!");
+                    }
+                    
+                }
+                
+            }
+            else if (x == "l")
+            {
+                Console.WriteLine("Minkä lämmön haluat asettaa? 30-70 astetta.");
+                while (true) //varmistaa että saa tuloksen
+                {
+                    var y = Console.ReadLine();
+                    if (int.TryParse(y, out int z))
+                    {
+                        z = Math.Clamp(z, 30, 70);
+                        Console.WriteLine("Lämpö on nyt " + z + " astetta.");
+                        kiuas.temp = z;
+                        Console.WriteLine("Oletko tämä sopiva? (y)");
+                        var b = Console.ReadLine();
+                        if (b == "y") { break; }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vain numeroita kiitos!");
+                    }
+                }
+            }
+            Console.WriteLine("Oletko valmis? (y)"); //(haluatko lopettaa sovelluksen)
+            var c = Console.ReadLine();
+            if (c == "y") { a = true; Console.WriteLine("Lämpötila on " + kiuas.temp + " astetta ja kosteus on " + kiuas.moist + "%."); }
+            else
+            {
+                Console.WriteLine("Minkä haluat asettaa (l)ämpötilan vai (K)osteuden?");
+            }
+        }
+    }
+}
